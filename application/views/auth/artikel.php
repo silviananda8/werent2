d395680ff6e534fb095bb4ed02ecf1db6ebe0db0
Rahadian-Hanif <63276849+Rahadian-Hanif@users.noreply.github.com>
2020-04-30 20:46:30 +0700

    halaman artikel

---
+++ application/views/auth/artikel.php
@@ -0,0 +1,99 @@
+<!-- isi konten -->
+<div class="container-fluid bg-dark mt-0">
+	<div class="container card-group">
+		<div class="col-lg-12">
+            <div class="section-title  text-white text-center">
+                <h2>Trending</h2>
+            </div>
+        </div>
+        <!--card-->
+        <div class="card" style="width: 18rem;">
+		  <img class="card-img-top" src="<?= base_url('assets/img/') ?>b1.jpg" alt="Card image cap">
+		  <div class="card-body">
+		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+		  </div>
+		</div>
+		<!--card-->
+        <div class="card" style="width: 18rem;">
+		  <img class="card-img-top" src="<?= base_url('assets/img/') ?>b2.jpg" alt="Card image cap">
+		  <div class="card-body">
+		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+		  </div>
+		</div>
+		<!--card-->
+        <div class="card" style="width: 18rem;">
+		  <img class="card-img-top" src="<?= base_url('assets/img/') ?>b3.jpg" alt="Card image cap">
+		  <div class="card-body">
+		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+		  </div>
+		</div>
+	</div>		
+	<div class="container mt-3">
+		 <div class="col-lg-12">
+            <div class="section-title  text-white text-center">
+                <h2>Tips dan Artikel</h2>
+            </div>
+        </div>
+		<!--card 1-->
+		<div class="card mb-3 ">
+			<div class="row no-gutters">
+				<div class="col-md-4">
+	      			<img src="<?= base_url('assets/img/') ?>b1.jpg" class="card-img" alt="...">
+	    		</div>
+	    		<div class="col-md-8">
+	    			<div class="card-body">
+					    <h5 class="card-title">Special title treatment</h5>
+					    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+					    <a href="#" class="btn btn-primary">See more</a>
+					</div>
+	    		</div>
+			</div>		  
+		</div>
+		<!--card 2-->
+		<div class="card mb-3">
+			<div class="row no-gutters">
+				<div class="col-md-4">
+	      			<img src="<?= base_url('assets/img/') ?>b3.jpg" class="card-img" alt="...">
+	    		</div>
+	    		<div class="col-md-8">
+	    			<div class="card-body">
+					    <h5 class="card-title">Special title treatment</h5>
+					    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+					    <a href="#" class="btn btn-primary">See more</a>
+					</div>
+	    		</div>
+			</div>		  
+		</div>
+		<!--card 3-->
+		<div class="card mb-3">
+			<div class="row no-gutters">
+				<div class="col-md-4">
+	      			<img src="<?= base_url('assets/img/') ?>b2.jpg" class="card-img" alt="...">
+	    		</div>
+	    		<div class="col-md-8">
+	    			<div class="card-body">
+					    <h5 class="card-title">Special title treatment</h5>
+					    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+					    <a href="#" class="btn btn-primary">See more</a>
+					</div>
+	    		</div>
+			</div>		  
+		</div>
+		<!--card 4-->
+		<div class="card mb-3">
+			<div class="row no-gutters">
+				<div class="col-md-4">
+	      			<img src="<?= base_url('assets/img/') ?>b4.jpg" class="card-img" alt="...">
+	    		</div>
+	    		<div class="col-md-8">
+	    			<div class="card-body">
+					    <h5 class="card-title">Special title treatment</h5>
+					    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+					    <a href="#" class="btn btn-primary">See more</a>
+					</div>
+	    		</div>
+			</div>		  
+		</div>
+		<!--end card-->
+	</div>	
+</div>
